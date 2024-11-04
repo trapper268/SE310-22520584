49f59a280943d91e7d48615c1d77f75b036c733a
huytt268 <tuanhuytruong35@gmail.com>
2024-11-04 14:21:08 +0700

    feat: add organi website

---
+++ ThucHanhWebMVC/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,117 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ThucHanhWebMVC.Models;
+using X.PagedList;
+
+namespace ThucHanhWebMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = (int)((page == null || page < 0) ? 1 : page);
+            var listsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(listsanpham, pageNumber, pageSize);
+            return View(lst);
+        }
+
+        [Route("ChiTietSanPham")]
+        public IActionResult ChiTietSanPham(string maSanPham)
+        {
+            var sanPham = db.TDanhMucSps.Find(maSanPham);
+            return View(sanPham);
+        }
+
+        [Route("ThemSanPhamMoi")]
+        [HttpGet]
+        public IActionResult ThemSanPhamMoi()
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+
+            return View();
+        }
+        [Route("ThemSanPhamMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemSanPhamMoi(TDanhMucSp sanPham)
+        {
+            if(ModelState.IsValid)
+            {
+                db.TDanhMucSps.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham");
+            }
+            return View(sanPham);
+        }
+
+        [Route("SuaSanPham")]
+        [HttpGet]
+        public IActionResult SuaSanPham(string maSanPham)
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+            var sanPham = db.TDanhMucSps.Find(maSanPham);
+            return View(sanPham);
+        }
+        [Route("SuaSanPham")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaSanPham(TDanhMucSp sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+            }
+            return View(sanPham);
+        }
+
+        
+
+        [Route("XoaSanPham")]
+        [HttpGet]
+        public IActionResult XoaSanPham(string maSanPham)
+        {
+            TempData["Message"] = "";
+            var chiTietSanPham = db.TChiTietSanPhams.Where(x => x.MaSp==maSanPham).ToList();
+            if(chiTietSanPham.Count() > 0)
+            {
+                TempData["Message"] = "Không xóa được sản phẩm này";
+                return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+            }
+            var anhSanPhams = db.TAnhSps.Where(x => x.MaSp == maSanPham);
+            if (anhSanPhams.Any()) db.RemoveRange(anhSanPhams);
+            db.Remove(db.TDanhMucSps.Find(maSanPham));
+            db.SaveChanges();
+            TempData["Message"] = "Sản phẩm đã được xóa";
+            return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+        }
+    }
+}
---
+++ ThucHanhWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using ThucHanhWebMVC.Models;
+using ThucHanhWebMVC.Models.Authentication;
+using ThucHanhWebMVC.ViewModels;
+using X.PagedList;
+
+namespace ThucHanhWebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Authentication]
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = (int)((page == null || page < 0) ? 1 : page);
+            var listsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x=>x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(listsanpham, pageNumber, pageSize);
+            return View(lst);
+        }
+        //[Authentication]
+        public IActionResult SanPhamTheoLoai(String maloai, int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = (int)((page == null || page < 0) ? 1 : page);
+            var listsanpham = db.TDanhMucSps.AsNoTracking().Where(x => x.MaLoai == maloai).OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(listsanpham, pageNumber, pageSize);
+            ViewBag.MaLoai = maloai;
+            return View(lst);
+        }
+
+        public IActionResult ChiTietSanPham(string maSp)
+        {
+            var sanPham = db.TDanhMucSps.SingleOrDefault(x=>x.MaSp == maSp);
+            var anhSanPham=db.TAnhSps.Where(x=>x.MaSp==maSp).ToList();
+            ViewBag.anhSanPham = anhSanPham;
+            return View(sanPham);
+        }
+
+        public IActionResult ProductDetail(string maSp)
+        {
+            var sanPham = db.TDanhMucSps.SingleOrDefault(x => x.MaSp == maSp);
+            var anhSanPham = db.TAnhSps.Where(x => x.MaSp == maSp).ToList();
+            var homeProductDetailViewModel=new HomeProductDetailViewModel { 
+                danhMucSp = sanPham, 
+                anhSps = anhSanPham 
+            };
+            return View(homeProductDetailViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
